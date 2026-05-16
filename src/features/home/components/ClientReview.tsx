f07b3ed3c596cdd2a05ca053d609f793c945ec99
Md Sakib Mia <sakib01181@gmail.com>
2026-05-16 15:59:38 +0600

    Travel Package Types Added

---
+++ src/features/home/components/ClientReview.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ClientReview() {
+  return (
+    <div>ClientReview</div>
+  )
+}
---
+++ src/types/home/types.ts
@@ -0,0 +1,9 @@
+export interface TravelPackage {
+  id: number;
+  country: string;
+  cities: string[];
+  duration: string;
+  price: string;
+  image: string;
+  agency: number;
+}
