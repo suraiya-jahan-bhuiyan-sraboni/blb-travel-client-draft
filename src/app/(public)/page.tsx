374ec170bd726dc45ea61d540a59d4ae78957dbf
Md Sakib Mia <sakib01181@gmail.com>
2026-05-18 20:25:47 +0600

    Travel Gallery comp added

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,6 +1,7 @@
 import ClientReview from "@/features/home/components/ClientReview";
 import Feedback from "@/features/home/components/FeedBack";
 import PackageList from "@/features/home/components/PackageList";
+import TravelGallery from "@/features/home/components/TravelGallery";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <PackageList />
       <ClientReview />
       <Feedback />
+      <TravelGallery />
     </main>
   );
 }
