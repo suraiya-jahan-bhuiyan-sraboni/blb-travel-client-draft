2b6659cdc7da07cb17144374ad86df67c037cfc8
dipto230 <diptoroy230305@gmail.com>
2026-05-20 04:55:34 +0530

    fetal:newsletter section updated like figma

--- src/features/home/components/NewsletterSection.tsx
+++ src/features/home/components/NewsletterSection.tsx
@@ -1,15 +1,43 @@
 const NewsletterSection = () => {
   return (
-    <section className="bg-[#F3F3F3] py-[90px] sm:py-[110px] lg:py-[130px]">
-      <div className="mx-auto flex max-w-[900px] flex-col items-center px-6 text-center">
+    <section className="bg-[#F3F3F3] py-[130px]">
+      <div className="mx-auto flex w-full max-w-[1440px] flex-col items-center px-6 text-center">
         
-        <h2 className="font-switzer text-[80px] font-extralight leading-[100px] tracking-[2.5px] text-[#1E293B]">
-          Travel
+   
+        <h2
+          className="
+            text-center
+            text-[80px]
+            font-[200]
+            leading-[100px]
+            tracking-[2.5px]
+            text-[#1E293B]
+            capitalize
+          "
+          style={{
+            fontFamily: "Switzer",
+          }}
+        >
+          TRAVEL
           <br />
-          Partner
+          PARTNER
         </h2>
 
-        <p className="mt-6 font-switzer text-[40px] font-light leading-[48px] text-[#94A3B8]">
+      
+        <p
+          className="
+            mt-[24px]
+            max-w-[900px]
+            text-center
+            text-[40px]
+            font-[200]
+            leading-[48px]
+            text-[#64748B]
+          "
+          style={{
+            fontFamily: "Switzer",
+          }}
+        >
           Discover travel inspiration based on your
           <br />
           preferred style.
